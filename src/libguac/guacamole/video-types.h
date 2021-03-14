aec4eec97d494c83b51bf29a986f845295fd7e7e
Michael Jumper <mjumper@apache.org>
2021-03-14 15:01:57 -0700

    WIP: Add libguac support for encoding video streams with FFMpeg.

---
+++ src/libguac/guacamole/video-types.h
@@ -0,0 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef GUAC_VIDEO_TYPES_H
+#define GUAC_VIDEO_TYPES_H
+
+/**
+ * A video which is actively being encoded. Frames can be added to the video
+ * as they are generated, along with their associated timestamps, and the
+ * corresponding video will be continuously written as it is encoded.
+ */
+typedef struct guac_video guac_video;
+
+#endif
+
